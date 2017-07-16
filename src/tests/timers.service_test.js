58e56c92ccb0ca7c05876b7de8c8ffb9a685fc5e
William Myers <william.myers@honestbee.com>
2017-07-17 05:54:50 +0800

    basic timers service unit tests

--- src/tests/timers.service_test.js
+++ src/tests/timers.service_test.js
@@ -1,43 +1,32 @@
 import sinon from 'sinon';
 import {expect} from 'chai';
-import {timers} from '../service';
+import service from '../service';
 
-describe('service', () => {
-  describe('#timers service', () => {
-    it('calls the api client with an url', () => {
-      const url = 'some.txt';
-      const apiClient = {
-        get: sinon.spy(() => Promise.resolve())
-      };
+describe('timers service', () => {
+  const apiClient = {
+    post: sinon.spy(() => Promise.resolve()),
+    patch: sinon.spy(() => Promise.resolve()),
+    delete: sinon.spy(() => Promise.resolve())
+  };
 
-      timers(apiClient).getTimers(url);
-      expect(apiClient.get.calledOnce).to.be.true;
-      expect(apiClient.get.calledWithExactly(url)).to.be.true;
+  const timersService = service(apiClient).timers;
+
+  describe('#add counter', () => {
+    it('calls the api client post method', () => {
+      timersService.addCounter();
+      expect(apiClient.post.calledOnce).to.be.true;
     });
-
-    it('does not call the api client for a repeat fetch, but returns cached data', (done) => {
-
-      const apiResponse = {}; // return an object to confirm equality
-      const apiClient = {
-        get: sinon.spy(() => Promise.resolve(apiResponse))
-      };
-      const url = 'some.txt';
-      const timersService = timers(apiClient);
-
-      let response1, response2;
-
-      timersService.getTimers(url)
-      .then(res1 => {
-        response1 = res1;
-        return timersService.getTimers(url);
-      })
-      .then(res2 => {
-        response2 = res2;
-        expect(apiClient.get.calledOnce).to.be.true;
-        expect(apiClient.get.calledTwice).to.be.false;
-        expect(response2).to.equal(response1);
-        done();
-      });
+  });
+  describe('#edit counter', () => {
+    it('calls the api client patch method', () => {
+      timersService.editCounter();
+      expect(apiClient.patch.calledOnce).to.be.true;
+    });
+  });
+  describe('#delete counter', () => {
+    it('calls the api client delete method', () => {
+      timersService.deleteCounter();
+      expect(apiClient.delete.calledOnce).to.be.true;
     });
   });
 });
